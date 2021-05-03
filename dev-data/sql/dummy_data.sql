9f3add94afc48db1297571d9133993b350a1893d
jiyoonhur <jiyoon1156@gmail.com>
2021-05-03 14:57:19 +0900

    [FEAT] Dummy data 수정
    데이터베이스 생성하는 sql 쿼리 추가

--- dev-data/sql/dummy_data.sql
+++ dev-data/sql/dummy_data.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `routinder` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `routinder`;
 -- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
 --
 -- Host: 127.0.0.1    Database: routinder
@@ -140,4 +142,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-03 14:32:11
+-- Dump completed on 2021-05-03 14:49:15
