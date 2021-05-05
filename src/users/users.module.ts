d4d523e51fe106a56e164fc5a8058e6c79eefd7e
SoHyun <pje1740@gmail.com>
2021-05-05 13:34:23 +0900

    [FEAT] user 테이블 연결 및 쿼리 생성 #3

---
+++ src/users/users.module.ts
@@ -0,0 +1,11 @@
+import { User } from './entities/user.entity';
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersResolver } from './users.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersResolver, UsersService],
+})
+export class UsersModule {}
