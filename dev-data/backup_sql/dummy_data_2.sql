e4b5233f132b58201112857d7f8eb3e5420af301
jiyoonhur <jiyoon1156@gmail.com>
2021-12-05 18:08:00 +0900

    [CHORE] Update dummy data
    - dummy data를 업데이트

--- dev-data/backup_sql/dummy_data_2.sql
+++ dev-data/backup_sql/dummy_data_2.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE  IF NOT EXISTS `routinder` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `routinder`;
--- MySQL dump 10.13  Distrib 8.0.22, for macos10.15 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
 --
 -- Host: 127.0.0.1    Database: routinder
 -- ------------------------------------------------------
@@ -26,7 +26,7 @@ DROP TABLE IF EXISTS `routine`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `routine` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `userId` int NOT NULL,
+  `userId` int DEFAULT NULL,
   `title` varchar(255) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `startDate` timestamp NOT NULL,
@@ -35,8 +35,8 @@ CREATE TABLE `routine` (
   `stickerId` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_c45924a1b42b6620e435e677cb5` (`userId`),
-  CONSTRAINT `FK_c45924a1b42b6620e435e677cb5` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `FK_c45924a1b42b6620e435e677cb5` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ CREATE TABLE `routine` (
 
 LOCK TABLES `routine` WRITE;
 /*!40000 ALTER TABLE `routine` DISABLE KEYS */;
-INSERT INTO `routine` VALUES (1,1,'근력운동','2021-05-03 05:17:30','2021-04-01 00:00:00','2021-05-01 00:00:00','0123456',1),(2,1,'블로그쓰기','2021-05-03 05:24:09','2021-03-01 00:00:00','2021-06-01 00:00:00','5',2),(3,1,'근력운동','2021-05-03 05:24:09','2021-03-17 00:00:00','2021-05-23 00:00:00','024',3),(5,1,'코딩하기','2021-12-05 07:28:56','2021-12-04 00:10:11','2022-01-01 00:00:00','01234',1);
+INSERT INTO `routine` VALUES (1,1,'스트레칭','2021-05-03 05:17:30','2021-04-01 00:00:00','2021-05-01 00:00:00','0123456',1),(2,1,'블로그쓰기','2021-05-03 05:24:09','2021-03-01 00:00:00','2021-06-01 00:00:00','5',2),(3,1,'근력운동','2021-05-03 05:24:09','2021-03-17 00:00:00','2021-05-23 00:00:00','024',3);
 /*!40000 ALTER TABLE `routine` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,9 +90,9 @@ CREATE TABLE `sticker_stamp` (
   PRIMARY KEY (`id`),
   KEY `FK_d7709478298c84c6ada56ea4fbb` (`userId`),
   KEY `FK_9fe820d19c200cd3c23719d5672` (`routineId`),
-  CONSTRAINT `FK_9fe820d19c200cd3c23719d5672` FOREIGN KEY (`routineId`) REFERENCES `routine` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `FK_d7709478298c84c6ada56ea4fbb` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `FK_9fe820d19c200cd3c23719d5672` FOREIGN KEY (`routineId`) REFERENCES `routine` (`id`),
+  CONSTRAINT `FK_d7709478298c84c6ada56ea4fbb` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,7 +101,7 @@ CREATE TABLE `sticker_stamp` (
 
 LOCK TABLES `sticker_stamp` WRITE;
 /*!40000 ALTER TABLE `sticker_stamp` DISABLE KEYS */;
-INSERT INTO `sticker_stamp` VALUES (1,1,1,0,NULL,'2021-04-21 00:00:00'),(2,1,1,0,NULL,'2021-04-21 00:00:00'),(3,1,2,0,NULL,'2021-04-21 00:00:00'),(4,1,3,1,'2021-03-29 00:00:00','2021-04-21 00:00:00');
+INSERT INTO `sticker_stamp` VALUES (1,1,1,1,'2021-04-02 17:00:00','2021-04-21 00:00:00'),(2,1,1,0,NULL,'2021-04-21 00:00:00'),(3,1,2,0,NULL,'2021-04-21 00:00:00'),(4,1,3,1,'2021-03-29 00:00:00','2021-04-21 00:00:00');
 /*!40000 ALTER TABLE `sticker_stamp` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -138,4 +138,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-05 18:05:16
+-- Dump completed on 2021-07-06 17:58:26
