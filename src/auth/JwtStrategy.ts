677555b829bddf9aaaea5dc37a09dbc7f6ebea4b
Sohyun Park <sohyun.grace.park@navercorp.com>
2022-09-11 22:19:37 +0900

    feat    : JwtStrategy cookie 에서 추출하도록 수정 (#26)

--- src/auth/JwtStrategy.ts
+++ src/auth/JwtStrategy.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-jwt';
+import * as cookie from 'cookie';
 
+const extractJWTFromCookie = (req) => {
+  let jwt = null;
+
+  const cookies = req.headers.cookie || req.headers.cookies;
+  if (req && cookies) {
+    const { jwtToken } = cookie.parse(cookies || '');
+    jwt = jwtToken;
+  }
+
+  return jwt;
+};
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: extractJWTFromCookie,
       ignoreExpiration: false,
       secretOrKey: process.env.SECRET,
     });
